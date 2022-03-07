b6706b8a100a10b888461e203ad60af4a3e9ad33
Mitchell <BitcoinMitchell@users.noreply.github.com>
2022-03-07 19:23:36 +0100

    Updated version

--- modules/btcpay/btcpay.php
+++ modules/btcpay/btcpay.php
@@ -55,7 +55,7 @@ class BTCPay extends PaymentModule
 	{
 		$this->name                   = 'btcpay';
 		$this->tab                    = 'payments_gateways';
-		$this->version                = '5.1.1';
+		$this->version                = '5.1.2';
 		$this->author                 = 'BTCPay Server';
 		$this->ps_versions_compliancy = ['min' => Constants::MINIMUM_PS_VERSION, 'max' => _PS_VERSION_];
 		$this->controllers            = ['webhook', 'payment', 'validation'];
