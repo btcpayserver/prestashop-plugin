79499ae5a57cb62e7f58e43eb9bc78ea36cdb3c3
Mitchell <BitcoinMitchell@users.noreply.github.com>
2023-07-11 23:48:21 +0200

    Use a null check instead an empty check

--- modules/btcpay/src/Server/Webhook.php
+++ modules/btcpay/src/Server/Webhook.php
@@ -35,7 +35,7 @@ class Webhook extends \BTCPayServer\Client\Webhook
 	public function ensureWebhook(string $storeId): void
 	{
 		// Check if we have an existing webhook, if so, just cancel now
-		if (false === empty($this->getCurrent($storeId, $this->configuration->get(Constants::CONFIGURATION_BTCPAY_WEBHOOK_ID)))) {
+		if (null === $this->getCurrent($storeId, $this->configuration->get(Constants::CONFIGURATION_BTCPAY_WEBHOOK_ID))) {
 			return;
 		}
 
